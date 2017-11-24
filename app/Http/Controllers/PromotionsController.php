b96039221a3ff44665dc4eeffbc0706f27eff0e8
Dzingwa01 <tongaichiridza@gmail.com>
2017-11-24 10:39:54 +0200

    Promotions queries for beer and establishment names

--- app/Http/Controllers/PromotionsController.php
+++ app/Http/Controllers/PromotionsController.php
@@ -64,17 +64,22 @@ class PromotionsController extends Controller
 
     public function apiPromotions()
     {
-        return Promotion::all();
+        $promotions = DB::table('promotions')
+            ->join('establishments', 'establishments.id', 'promotions.establishment_id')
+            ->join('beers', 'beers.id', 'promotions.beer_id')
+            ->select('title', 'promotions.id', 'start_date', 'end_date', 'promotions.beer_id', 'promotions.establishment_id', 'promotions.status', 'promotions.price', 'beers.name as beer_name', 'establishments.name as establishment_name')->get();
+//        dd($promotions);
+        return response()->json($promotions);
     }
 
     public function apiPromotion($establishment_id)
     {
-        $promotion = Promotion::where('establishment_id', $establishment_id)->get();
-        if ($promotion != null) {
-            return $promotion;
-        } else {
-            return [];
-        }
+        $promotion = Promotion::join('establishments', 'establishments.id', 'promotions.establishment_id')
+            ->join('beers', 'beers.id', 'promotions.beer_id')
+            ->where('establishment_id', $establishment_id)
+            ->select('title', 'promotions.id', 'start_date', 'end_date', 'promotions.beer_id', 'promotions.establishment_id', 'promotions.status', 'promotions.price', 'beers.name as beer_name', 'establishments.name as establishment_name')->get();
+//
+        return response()->json($promotion);
 
     }
 
